68139e268de6c1697b27352c5a26a765902c6bb7
Matt Johnson <mahdisamieefarisar@outlook.com>
2021-03-08 22:29:29 +0330

    change dev.js file and add menu btn button

--- dev.js
+++ dev.js
@@ -7,8 +7,10 @@ sideNav.style.right = "-25rem";
 sideNavBtn.onclick = function () {
   if (sideNav.style.right == "-25rem") {
     sideNav.style.right = "0rem";
+    sideNavBtn.src = "img/closeBtn.png";
   } else {
     sideNav.style.right = "-25rem";
+    sideNavBtn.src = "img/menu.png";
   }
 };
 /* 
